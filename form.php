f9952f4516a2197ec5930a7d1c5c53c830f5983d
Vikas Kumar <59094821+Vikaskumar990@users.noreply.github.com>
2020-09-20 16:17:09 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,38 @@
+ <header class="header-login top-page">
+      <div class="container">
+	   <div class="content">
+	    <div class="row">
+	     <h1 class="revealOnScroll" data-animation="fadeInDown">WorkHire</h1>
+        </div><!-- /.row -->
+       </div><!-- /.content -->
+	  </div><!-- /.container -->
+     </header><!-- /header -->
+	 
+     <!-- ==============================================
+     Banner Login Section
+     =============================================== -->
+	 <section class="banner-login">
+	  <div class="container">
+	  		  	
+	   <div class="row">
+	   
+	    <div class="main main-signup col-lg-12">
+	     <div class="col-lg-6 col-lg-offset-3 text-center">
+						<div class="row">
+							<div class="col-md-12">
+								<?=form_open($action);?>                                    
+			                        <?=$FormInput;?>
+			                        <div class="form-group">
+			                                <input type="submit" class="btn btn-info" value="Register" style="width: 100%;">
+									</div>
+			                    <?=form_close();?>
+							</div>
+						</div>
+		  </div><!-- /.col-lg-6 -->
+        </div>
+		
+	   </div><!-- /.row -->
+	  </div><!-- /.container -->
+    </section><!-- /section --> 			
+
+
