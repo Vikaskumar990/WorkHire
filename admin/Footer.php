327355921e50487724077891d6aa77fdaf36b399
Vikas Kumar <59094821+Vikaskumar990@users.noreply.github.com>
2020-10-13 22:30:50 +0530

    Add files via upload

---
+++ admin/Footer.php
@@ -0,0 +1,259 @@
+                <footer class="footer text-right">
+                    2019 © WorkHire.
+                </footer>
+
+            </div>
+
+
+            <!-- ============================================================== -->
+            <!-- End Right content here -->
+            <!-- ============================================================== -->
+
+
+            <!-- Right Sidebar -->
+            <div class="side-bar right-bar nicescroll">
+                <h4 class="text-center">Chat</h4>
+                <div class="contact-list nicescroll">
+                    <ul class="list-group contacts-list">
+                        <li class="list-group-item">
+                            <a href="#">
+                                <div class="avatar">
+                                    <img src="<?=base_url();?>Assets/AD_assets/images/users/avatar-1.jpg" alt="">
+                                </div>
+                                <span class="name">Chadengle</span>
+                                <i class="fa fa-circle online"></i>
+                            </a>
+                            <span class="clearfix"></span>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#">
+                                <div class="avatar">
+                                    <img src="<?=base_url();?>Assets/AD_assets/images/users/avatar-2.jpg" alt="">
+                                </div>
+                                <span class="name">Tomaslau</span>
+                                <i class="fa fa-circle online"></i>
+                            </a>
+                            <span class="clearfix"></span>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#">
+                                <div class="avatar">
+                                    <img src="<?=base_url();?>Assets/AD_assets/images/users/avatar-3.jpg" alt="">
+                                </div>
+                                <span class="name">Stillnotdavid</span>
+                                <i class="fa fa-circle online"></i>
+                            </a>
+                            <span class="clearfix"></span>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#">
+                                <div class="avatar">
+                                    <img src="<?=base_url();?>Assets/AD_assets/images/users/avatar-4.jpg" alt="">
+                                </div>
+                                <span class="name">Kurafire</span>
+                                <i class="fa fa-circle online"></i>
+                            </a>
+                            <span class="clearfix"></span>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#">
+                                <div class="avatar">
+                                    <img src="<?=base_url();?>Assets/AD_assets/images/users/avatar-5.jpg" alt="">
+                                </div>
+                                <span class="name">Shahedk</span>
+                                <i class="fa fa-circle away"></i>
+                            </a>
+                            <span class="clearfix"></span>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#">
+                                <div class="avatar">
+                                    <img src="<?=base_url();?>Assets/AD_assets/images/users/avatar-6.jpg" alt="">
+                                </div>
+                                <span class="name">Adhamdannaway</span>
+                                <i class="fa fa-circle away"></i>
+                            </a>
+                            <span class="clearfix"></span>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#">
+                                <div class="avatar">
+                                    <img src="<?=base_url();?>Assets/AD_assets/images/users/avatar-7.jpg" alt="">
+                                </div>
+                                <span class="name">Ok</span>
+                                <i class="fa fa-circle away"></i>
+                            </a>
+                            <span class="clearfix"></span>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#">
+                                <div class="avatar">
+                                    <img src="<?=base_url();?>Assets/AD_assets/images/users/avatar-8.jpg" alt="">
+                                </div>
+                                <span class="name">Arashasghari</span>
+                                <i class="fa fa-circle offline"></i>
+                            </a>
+                            <span class="clearfix"></span>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#">
+                                <div class="avatar">
+                                    <img src="<?=base_url();?>Assets/AD_assets/images/users/avatar-9.jpg" alt="">
+                                </div>
+                                <span class="name">Joshaustin</span>
+                                <i class="fa fa-circle offline"></i>
+                            </a>
+                            <span class="clearfix"></span>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#">
+                                <div class="avatar">
+                                    <img src="<?=base_url();?>Assets/AD_assets/images/users/avatar-10.jpg" alt="">
+                                </div>
+                                <span class="name">Sortino</span>
+                                <i class="fa fa-circle offline"></i>
+                            </a>
+                            <span class="clearfix"></span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /Right-bar -->
+
+        </div>
+        <!-- END wrapper -->
+
+
+
+        <script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+         <!-- jQuery  -->
+       
+     
+
+     
+        <script src="<?=base_url();?>Assets/AD_assets/js/jquery.min.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/js/bootstrap.min.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/js/detect.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/js/fastclick.js"></script>
+
+        <script src="<?=base_url();?>Assets/AD_assets/js/jquery.slimscroll.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/js/jquery.blockUI.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/js/waves.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/js/wow.min.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/js/jquery.nicescroll.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/js/jquery.scrollTo.min.js"></script>
+
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/peity/jquery.peity.min.js"></script>
+
+        <!-- jQuery  -->
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/waypoints/lib/jquery.waypoints.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/counterup/jquery.counterup.min.js"></script>
+
+        <!--datatable  -->
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/bootstrap-table/dist/bootstrap-table.min.js"></script>
+
+        <script src="<?=base_url();?>Assets/AD_assets/pages/jquery.bs-table.js"></script>
+
+
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/morris/morris.min.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/raphael/raphael-min.js"></script>
+
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/jquery-knob/jquery.knob.js"></script>
+
+        <script src="<?=base_url();?>Assets/AD_assets/pages/jquery.dashboard.js"></script>
+
+        <script src="<?=base_url();?>Assets/AD_assets/js/jquery.core.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/js/jquery.app.js"></script>
+         <!-- new -->
+           <script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+        
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/datatables/jquery.dataTables.min.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/datatables/dataTables.bootstrap.js"></script>
+
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/datatables/dataTables.buttons.min.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/datatables/buttons.bootstrap.min.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/datatables/jszip.min.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/datatables/pdfmake.min.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/datatables/vfs_fonts.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/datatables/buttons.html5.min.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/datatables/buttons.print.min.js"></script>
+
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/datatables/dataTables.fixedHeader.min.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/datatables/dataTables.keyTable.min.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/datatables/dataTables.responsive.min.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/datatables/responsive.bootstrap.min.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/datatables/dataTables.scroller.min.js"></script>
+
+        <script src="<?=base_url();?>Assets/AD_assets/pages/datatables.init.js"></script>
+
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $('#datatable').dataTable();
+                $('#datatable-keytable').DataTable( { keys: true } );
+                $('#datatable-responsive').DataTable();
+                $('#datatable-scroller').DataTable( { ajax: "assets/plugins/datatables/json/scroller-demo.json", deferRender: true, scrollY: 380, scrollCollapse: true, scroller: true } );
+                var table = $('#datatable-fixed-header').DataTable( { fixedHeader: true } );
+            } );
+            TableManageButtons.init();
+
+        </script>
+
+   
+         <!-- new -->   
+
+
+
+
+
+
+        <!-- Sweet-Alert  -->
+        <script src="<?=base_url();?>Assets/AD_assets/plugins/sweetalert/dist/sweetalert.min.js"></script>
+        <script src="<?=base_url();?>Assets/AD_assets/js/swal.js"></script>
+
+        
+
+
+
+        <script type="text/javascript">
+            <?php $value=$this->session->flashdata('value');
+            if($value=="p"){?>
+                swal("Not Valid","Your Password","error");
+            <?php }elseif($value=="e"){?>
+                swal("Not Valid","Your Email","error");
+            <?php }elseif($value=="login"){?>
+                swal("Welcome","login Successfully","success");
+            <?php }elseif($value=="d"){?>
+                swal("Successfully","Deleted your data!!!","success");
+            <?php }elseif($value=="I"){?>
+                swal("Successfully","Inserted","success");
+            <?php }?>
+
+        </script>
+
+        <script type="text/javascript">
+            jQuery(document).ready(function($) {
+                $('.counter').counterUp({
+                    delay: 100,
+                    time: 1200
+                });
+
+                $(".knob").knob();
+
+            });
+        </script>
+
+
+
+
+    </body>
+
+<!-- Mirrored from coderthemes.com/ubold_1.2/light/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 13 Jan 2016 04:52:39 GMT -->
+</html>
